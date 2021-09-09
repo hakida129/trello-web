085b24d7ac406ffd4139295c896ce791bf5f6665
hakida129 <vietlongit@gmail.com>
2021-09-09 11:49:30 +0800

    Design trello front-ent.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,116 @@
 
-import './App.css';
+import './App.scss';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello World!</h1>
+    <div className="trello-master">
+      <nav className="navbar app">App Bar</nav>
+      <nav className="navbar board">App Board</nav>
+      <div className="board-columns">
+        <div className="column">
+          <header>Brainstorm</header>
+          <ul>
+            <li>
+              <img src="https://bloganh.net/wp-content/uploads/2021/03/chup-anh-dep-anh-sang-min.jpg" alt="" />
+              Title: Anh gaix xinh
+            </li>
+            <li>successfullysuccessfullysuccessfully!</li>
+            <li>successfullysuccessfullysuccessfully!</li>
+            <li>successfullysuccessfullysuccessfully!</li>
+            <li>successfullysuccessfullysuccessfully!</li>
+            <li>successfullysuccessfullysuccessfully!</li>
+            <li>successfullysuccessfullysuccessfully!</li>
+            <li>successfullysuccessfullysuccessfully!</li>
+            <li>successfullysuccessfullysuccessfully!</li>
+            <li>successfullysuccessfullysuccessfully!</li>
+          </ul>
+          <footer>Add another card</footer>
+        </div>
+        <div className="column">
+          <header>To do</header>
+          <ul>
+            <li>
+              <img src="https://bloganh.net/wp-content/uploads/2021/03/chup-anh-dep-anh-sang-min.jpg" alt="" />
+              Title: Anh gaix xinh
+            </li>
+            <li>successfullysuccessfullysuccessfully!</li>
+            <li>successfullysuccessfullysuccessfully!</li>
+            <li>successfullysuccessfullysuccessfully!</li>
+            <li>successfullysuccessfullysuccessfully!</li>
+          </ul>
+          <footer>Add another card</footer>
+        </div>
+        <div className="column">
+          <header>To do</header>
+          <ul>
+            <li>
+              <img src="https://bloganh.net/wp-content/uploads/2021/03/chup-anh-dep-anh-sang-min.jpg" alt="" />
+              Title: Anh gaix xinh
+            </li>
+            <li>successfullysuccessfullysuccessfully!</li>
+            <li>successfullysuccessfullysuccessfully!</li>
+            <li>successfullysuccessfullysuccessfully!</li>
+            <li>successfullysuccessfullysuccessfully!</li>
+          </ul>
+          <footer>Add another card</footer>
+        </div>
+        <div className="column">
+          <header>To do</header>
+          <ul>
+            <li>
+              <img src="https://bloganh.net/wp-content/uploads/2021/03/chup-anh-dep-anh-sang-min.jpg" alt="" />
+              Title: Anh gaix xinh
+            </li>
+            <li>successfullysuccessfullysuccessfully!</li>
+            <li>successfullysuccessfullysuccessfully!</li>
+            <li>successfullysuccessfullysuccessfully!</li>
+            <li>successfullysuccessfullysuccessfully!</li>
+          </ul>
+          <footer>Add another card</footer>
+        </div>
+        <div className="column">
+          <header>To do</header>
+          <ul>
+            <li>
+              <img src="https://bloganh.net/wp-content/uploads/2021/03/chup-anh-dep-anh-sang-min.jpg" alt="" />
+              Title: Anh gaix xinh
+            </li>
+            <li>successfullysuccessfullysuccessfully!</li>
+            <li>successfullysuccessfullysuccessfully!</li>
+            <li>successfullysuccessfullysuccessfully!</li>
+            <li>successfullysuccessfullysuccessfully!</li>
+          </ul>
+          <footer>Add another card</footer>
+        </div>
+        <div className="column">
+          <header>To do</header>
+          <ul>
+            <li>
+              <img src="https://bloganh.net/wp-content/uploads/2021/03/chup-anh-dep-anh-sang-min.jpg" alt="" />
+              Title: Anh gaix xinh
+            </li>
+            <li>successfullysuccessfullysuccessfully!</li>
+            <li>successfullysuccessfullysuccessfully!</li>
+            <li>successfullysuccessfullysuccessfully!</li>
+            <li>successfullysuccessfullysuccessfully!</li>
+          </ul>
+          <footer>Add another card</footer>
+        </div>
+        <div className="column">
+          <header>To do</header>
+          <ul>
+            <li>
+              <img src="https://bloganh.net/wp-content/uploads/2021/03/chup-anh-dep-anh-sang-min.jpg" alt="" />
+              Title: Anh gaix xinh
+            </li>
+            <li>successfullysuccessfullysuccessfully!</li>
+            <li>successfullysuccessfullysuccessfully!</li>
+            <li>successfullysuccessfullysuccessfully!</li>
+            <li>successfullysuccessfullysuccessfully!</li>
+          </ul>
+          <footer>Add another card</footer>
+        </div>
+      </div>
     </div>
   );
 }
