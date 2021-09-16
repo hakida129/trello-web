5f39a2608fbaf01da2567ccf1c202ab2040e7ecf
hakida129 <vietlongit@gmail.com>
2021-09-16 23:28:31 +0800

    Add new card to column.

--- src/components/BoardContent/BoardContent.js
+++ src/components/BoardContent/BoardContent.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef, useCallback } from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 import { Container, Draggable } from 'react-smooth-dnd'
 import { Container as BootstrapContainer, Row, Col, Form, Button } from 'react-bootstrap'
 import './BoardContent.scss'
@@ -13,11 +13,13 @@ function BoardContent() {
   const [ board, setBoard ] = useState({})
   const [ columns, setColumns ] = useState([])
   const [ openNewColumnForm, setOpenNewColumnForm ] = useState(false)
-
+  const toggleOpenNewColumnForm = () => {
+    setOpenNewColumnForm(!openNewColumnForm)
+  }
   const newColumnInputRef = useRef(null)
 
   const [newColumnTitle, setNewColumnTitle] = useState('')
-  const onNewColumnTitleChange = useCallback((e) => setNewColumnTitle(e.target.value), [])
+  const onNewColumnTitleChange = (e) => setNewColumnTitle(e.target.value)
 
   useEffect(() => {
     const boardFromDB = initialData.boards.find(board => board.id === 'board-1')
@@ -60,9 +62,7 @@ function BoardContent() {
     }
   }
 
-  const toggleOpenNewColumnForm = () => {
-    setOpenNewColumnForm(!openNewColumnForm)
-  }
+
 
   const addNewColumn = () => {
     if (!newColumnTitle) {
@@ -109,6 +109,7 @@ function BoardContent() {
     setColumns(newColumns)
     setBoard(newBoards)
   }
+
   return (
     <div className="board-content">
       <Container
@@ -123,7 +124,11 @@ function BoardContent() {
         }} >
         { columns.map((column, index) => (
           <Draggable key={index}>
-            <Column column={column} onCardDrop={onCardDrop} onUpdateColumn={onUpdateColumn} />
+            <Column
+              column={column}
+              onCardDrop={onCardDrop}
+              onUpdateColumn={onUpdateColumn}
+            />
           </Draggable>
         ))}
       </Container>
@@ -149,7 +154,7 @@ function BoardContent() {
                 onKeyDown={event => (event.key === 'Enter') && addNewColumn()}
               />
               <Button variant="success" size="sm" onClick={addNewColumn}>Add column</Button>
-              <span className="cancel-new-column" onClick={toggleOpenNewColumnForm}>
+              <span className="cancel-icon" onClick={toggleOpenNewColumnForm}>
                 <i className="fa fa-remove icon"></i>
               </span>
             </Col>
